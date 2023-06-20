bc71fa7b923dd2af62a6cfc5ee1dbe9896a325cb
sngom1995 <ngomsamba75@gmail.com>
2023-06-20 09:04:18 +0000

    usernameAvailable methode implementation

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -2,18 +2,19 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { AsyncValidator, AbstractControl } from '@angular/forms';
 import { Observable, catchError, map, of } from "rxjs";
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniqueUsername implements AsyncValidator {
 
-  constructor(private http: HttpClient){}
+  constructor(private authService:AuthService){}
 
   validate =(control: AbstractControl) =>{
     const { value } = control;
-    console.log(this.http);
-    return  this.http.post<any>('https://api.angular-email.com/auth/username', {username: value}).pipe(
+
+    return  this.authService.usernameAvailable(value).pipe(
       map((value) => {
       console.log(value);
       return null
