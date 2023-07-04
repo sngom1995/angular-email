545a4f64bb7f77539f8e7ec40159a6105b2b5c4f
sngom1995 <ngomsamba75@gmail.com>
2023-07-04 02:31:07 +0000

    Behavior Subject

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject} from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface UsernameAvailableResponse {
   available: boolean;
@@ -11,10 +13,16 @@ interface UsernameAvailableResponse {
 export class AuthService {
 
   constructor(private http: HttpClient) { }
-
+  signedin$ = new BehaviorSubject(false);
   usernameAvailable(username: string){
     return this.http.post<UsernameAvailableResponse>('https://api.angular-email.com/auth/username', {
       username
-    });
+    })
+    .pipe(
+      tap((response) => {
+        console.log(response);
+        this.signedin$.next(true);
+      })
+    );
   }
 }
